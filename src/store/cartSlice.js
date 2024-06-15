52f9545a299391309b455f3caa1040999db46512
Vineet Tiwari <43114880+Vineettiwari02@users.noreply.github.com>
2024-06-15 13:44:10 +0530

    redux news

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,29 +1,24 @@
-const { createSlice } = require("@reduxjs/toolkit");
+import { createSlice } from '@reduxjs/toolkit'
 
 const cartSlice = createSlice({
     name: "cart",
-
     initialState: {
         cartList: [],
         total: 0
     },
-
-    reducers:{
+    reducers: {
         add(state, action){
-            // console.log(action.payload)
             const updatedCartList = state.cartList.concat(action.payload);
             const total = state.total + action.payload.price;
-            return {...state,total: total, cartList:updatedCartList };
-
+            return {...state, total: total, cartList: updatedCartList};
         },
         remove(state, action){
-            // console.log(action.payload)
             const updatedCartList = state.cartList.filter(item => item.id !== action.payload.id);
             const total = state.total - action.payload.price;
-            return {...state,total: total, cartList:updatedCartList };
+            return {...state, total: total, cartList: updatedCartList};
         }
     }
 });
 
-export const {add, remove} = cartSlice.actions;
+export const { add, remove } = cartSlice.actions;
 export const cartReducer = cartSlice.reducer;
